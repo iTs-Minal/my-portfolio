d29c73373b91e14b9f4bfec6f2b2c4b8f93ffdd3
Minal Pudasainee <pudasaineeminal@gmail.com>
2025-01-29 21:37:08 +0545

    Portfolio completed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,23 +9,23 @@ export default {
   theme: {
     extend: {
       colors: {
-        lightHover:"#fcf4ff",
-        darkHover:"#2a004a",
-        darkTheme:"#11001F"
+        lightHover: "#fcf4ff",
+        darkHover: "#2a004a",
+        darkTheme: "#11001F"
       },
-      fontFamily:{
-        Outfit:["Outfit", "sans-serif"],
-        Ovo:["Ovo", "serif"]
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"]
       },
-      boxShadow:{
-        "black":"4px 4px 0 #000",
-         "white":"4px 4px 0 #fff"
+      boxShadow: {
+        "black": "4px 4px 0 #000",
+        "white": "4px 4px 0 #fff"
       },
-      gridTemplateColumns:{
-        "auto":"repeat(auto-fit,minmax(200px, 1fr))"
+      gridTemplateColumns: {
+        "auto": "repeat(auto-fit,minmax(200px, 1fr))"
       }
     },
   },
-  darkMode:"selector",
+  darkMode: "selector",
   plugins: [],
 } satisfies Config;
